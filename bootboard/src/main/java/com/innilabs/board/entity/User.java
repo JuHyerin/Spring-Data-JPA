15c7db11c1a061ae2697fbe7f60289b1bbc5d39d
JuHyerin <jhr67370719@gmail.com>
2020-11-06 19:20:23 +0900

    findAll 구현

--- bootboard/src/main/java/com/innilabs/board/entity/User.java
+++ bootboard/src/main/java/com/innilabs/board/entity/User.java
@@ -4,10 +4,8 @@ package com.innilabs.board.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
